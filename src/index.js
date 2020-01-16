6d7823d7e177a46aa4b1cc13bfbc63a0cdbb0c41
Cassio C <LccassioL@gmail.com>
2020-01-16 20:10:57 -0300

    Create project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/omnistack", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
