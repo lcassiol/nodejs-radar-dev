c588a780fa787c1f3b86a8ca8be9bad14af6f6cd
Cassio C <LccassioL@gmail.com>
2020-01-20 23:07:06 -0300

    Add search with techs case insensitive

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -4,13 +4,31 @@ const parseStringAsArray = require("../utils/parseStringAsArray");
 module.exports = {
   async index(request, response) {
     const { latitude, longitude, techs } = request.query;
-    const techsArray = parseStringAsArray(techs);
 
-    const devs = await Dev.find({
-      techs: {
-        $in: techsArray
+    let query = {
+      location: {
+        $nearSphere: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
       }
-    });
+    };
+
+    if (techs) {
+      const techsArray = parseStringAsArray(techs);
+
+      if (techsArray) {
+        query.techs = {
+          $in: new RegExp(`^${techsArray}$`, "i")
+        };
+      }
+    }
+
+    console.log(query);
+    const devs = await Dev.find(query);
 
     return response.json(devs);
   }
